bb8e0a1aba14d1a27dd9318e1d9e89c6acab6472
고범석 <rhqkf7496@naver.com>
2024-05-08 17:42:06 +0900

    2024-05-08

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
-import './App.css';
 import Customer from './components/Customer';
+import './App.css';
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import { withStyles } from "@mui/material/styles";
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
     'id': 1,
-    'image': 'http://placeimg.com/64/64/1',
+    'image': 'https://picsum.photos/id/2/200',
     'name': '고범석',
     'birthday': '971013',
     'gender': '남자',
@@ -13,7 +31,7 @@ const customers = [
   },
   {
     'id': 2,
-    'image': 'http://placeimg.com/64/64/2',
+    'image': 'https://picsum.photos/id/123/200',
     'name': '고범석2',
     'birthday': '971111',
     'gender': '여자',
@@ -21,7 +39,7 @@ const customers = [
   },
   {
     'id': 3,
-    'image': 'http://placeimg.com/64/64/3',
+    'image': 'https://picsum.photos/id/168/200',
     'name': '고범석3',
     'birthday': '970101',
     'gender': '남자',
@@ -31,26 +49,28 @@ const customers = [
 
 class App extends Component {
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-            );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />); })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
